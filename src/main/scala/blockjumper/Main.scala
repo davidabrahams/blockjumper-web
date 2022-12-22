4811836366095c6e7356c30ee981a36cb88d78eb
davidabrahams <da.j.abrahams@gmail.com>
2022-12-21 23:21:20 -0700

    Change window size (#5)
    * get the 2d context
    
    * update canvas size
    
    Co-authored-by: David Abrahams <davidabrahams@stripe.com>

--- src/main/scala/blockjumper/Main.scala
+++ src/main/scala/blockjumper/Main.scala
@@ -1,5 +1,15 @@
 package blockjumper
 
+import org.scalajs.dom.*
+
 object Main:
   def main(args: Array[String]): Unit =
-    println("Hello world!")
+    val context: CanvasRenderingContext2D = document
+      .querySelector("canvas")
+      .asInstanceOf[html.Canvas]
+      .getContext("2d")
+      .asInstanceOf
+    val canvas: HTMLCanvasElement = context.canvas
+    canvas.width = 1170
+    canvas.height = 900
+    println("Hello word!")
