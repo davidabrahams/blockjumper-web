e380e3ec7b2c9328905106418d9ccd31f3782f15
David Abrahams <davidabrahams@stripe.com>
2022-12-22 03:35:11 -0800

    refactor

--- src/main/scala/blockjumper/Main.scala
+++ src/main/scala/blockjumper/Main.scala
@@ -4,38 +4,51 @@ import org.scalajs.dom
 import scala.concurrent.duration.*
 
 case class GameState(soldier: Soldier):
-  def update(timeElapsed: Duration): GameState = GameState(
-    soldier.update(timeElapsed)
+  def update(timeElapsed: Duration, keyState: KeyState): GameState = GameState(
+    soldier.update(timeElapsed, keyState)
   )
   def draw(context: dom.CanvasRenderingContext2D): Unit = soldier.draw(context)
 
+object GameState:
+  val ScreenWidth = 800
+  val ScreenHeight = 600
+  val GrassHeight = 420
+
 case class Soldier(x: Double, y: Double, yVelocity: Double):
-  def update(timeElapsed: Duration): Soldier =
-    val afterFall = y + yVelocity * timeElapsed.toUnit(SECONDS)
-    if (afterFall > 420 - 84)
-      Soldier(
-        x,
-        420-84,
-        0
-      )
-    else
-      Soldier(
-        x,
-        afterFall,
-        yVelocity + Soldier.Gravity * timeElapsed.toUnit(SECONDS)
-      )
+  def update(timeElapsed: Duration, keyState: KeyState): Soldier =
+    val floor = GameState.GrassHeight - Soldier.Height
+    val newY = Math.min(floor, y + yVelocity * timeElapsed.toUnit(SECONDS))
+    val newX = (keyState.getLeftDown, keyState.getRightDown) match
+      case (true, false) =>
+        x - Soldier.WalkingSpeed * timeElapsed.toUnit(SECONDS)
+      case (false, true) =>
+        x + Soldier.WalkingSpeed * timeElapsed.toUnit(SECONDS)
+      case _ => x
+    Soldier(
+      newX,
+      newY,
+      if newY == floor then {
+        if keyState.getUpDown then -Soldier.JumpVelocity
+        else 0
+      } else yVelocity + Soldier.Gravity * timeElapsed.toUnit(SECONDS)
+    )
 
   def draw(context: dom.CanvasRenderingContext2D): Unit =
     context.drawImage(
       Soldier.image,
       x,
       y,
-      51,
-      84
+      Soldier.Width,
+      Soldier.Height
     )
 
 object Soldier:
   val Gravity = 5000 // original code had 2, running at 50 fps. 5000 = 2 * 50^2
+  val Width = 51
+  val Height = 84
+  val WalkingSpeed = 400 // original code had 8. 400 = 8 * 50
+  val JumpVelocity = 1400 // original code had 28. 1400 = 28 * 50
+
   val image =
     dom.document.createElement("img").asInstanceOf[dom.HTMLImageElement]
   image.src = "./soldier.png"
@@ -43,7 +56,8 @@ object Soldier:
 def animate(
     context: dom.CanvasRenderingContext2D,
     previousFrameTimestamp: Option[Double],
-    gameState: GameState
+    gameState: GameState,
+    keyState: KeyState
 ): Double => Unit = { (msTimestamp: Double) =>
   val timeElapsed: Duration = previousFrameTimestamp match
     case None       => Duration.Zero
@@ -51,15 +65,45 @@ def animate(
   assert(timeElapsed >= Duration.Zero)
   // draw the sky
   context.fillStyle = "#5FA6E7"
-  context.fillRect(0, 0, 800, 420) // x, y, width, height
+  context.fillRect(
+    0,
+    0,
+    GameState.ScreenWidth,
+    GameState.GrassHeight
+  ) // x, y, width, height
   // draw the grass
   context.fillStyle = "#3DB91F"
-  context.fillRect(0, 420, 800, 180)
+  context.fillRect(
+    0,
+    GameState.GrassHeight,
+    GameState.ScreenWidth,
+    GameState.ScreenHeight - GameState.GrassHeight
+  )
   gameState.draw(context)
   dom.window.requestAnimationFrame(
-    animate(context, Some(msTimestamp), gameState.update(timeElapsed))
+    animate(
+      context,
+      Some(msTimestamp),
+      gameState.update(timeElapsed, keyState),
+      keyState
+    )
   )
 }
+
+class KeyState(
+    private var leftDown: Boolean,
+    private var rightDown: Boolean,
+    private var upDown: Boolean
+):
+  def getLeftDown = leftDown
+  def getRightDown = rightDown
+  def getUpDown = upDown
+  def processKeyEvent(e: dom.KeyboardEvent, pressedDown: Boolean): Unit =
+    e.keyCode match
+      case 37 => leftDown = pressedDown
+      case 38 => upDown = pressedDown
+      case 39 => rightDown = pressedDown
+      case _  => ()
 
 @main def main(): Unit =
   val context: dom.CanvasRenderingContext2D = dom.document
@@ -68,8 +112,17 @@ def animate(
     .getContext("2d")
     .asInstanceOf
   val canvas: dom.HTMLCanvasElement = context.canvas
-  canvas.width = 800
-  canvas.height = 600
+  canvas.width = GameState.ScreenWidth
+  canvas.height = GameState.ScreenHeight
+  val keyState = KeyState(false, false, false)
+  dom.window.addEventListener("keydown", e => keyState.processKeyEvent(e, true))
+  dom.window.addEventListener("keyup", e => keyState.processKeyEvent(e, false))
   dom.window.requestAnimationFrame(
-    animate(context, None, GameState(Soldier(390, 0, 0)))
+    animate(
+      context,
+      None,
+      // TODO: maybe align based on the center hit line
+      GameState(Soldier(GameState.ScreenWidth / 2 - Soldier.Width / 2, 0, 0)),
+      keyState
+    )
   )
