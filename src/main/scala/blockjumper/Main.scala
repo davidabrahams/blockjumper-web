d2bd3e6365b1da5ab435595ff046c685b0c43dd3
David Abrahams <davidabrahams@stripe.com>
2022-12-21 17:11:45 -0800

    get the 2d context

--- src/main/scala/blockjumper/Main.scala
+++ src/main/scala/blockjumper/Main.scala
@@ -1,5 +1,10 @@
 package blockjumper
 
+import org.scalajs.dom.*
+
+
 object Main:
   def main(args: Array[String]): Unit =
+    val foo = document.querySelector("canvas").asInstanceOf[html.Canvas]
+    val context = foo.getContext("2d").asInstanceOf[CanvasRenderingContext2D]
     println("Hello world!")
