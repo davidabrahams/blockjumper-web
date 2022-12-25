9b4f458c814eb1605f1f8cce39121b500b7e99c6
David Abrahams <davidabrahams@stripe.com>
2022-12-24 23:28:29 -0800

    formatty

--- src/main/scala/blockjumper/Main.scala
+++ src/main/scala/blockjumper/Main.scala
@@ -76,7 +76,7 @@ class KeyState(
       case 32 => spaceDown = pressedDown
       case 88 =>
         xClicked |= pressedDown
-      case x  => ()
+      case x => ()
 
 @main def main(): Unit =
   val rng = util.Random()
