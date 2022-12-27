db92058e84c292a9a69cf15a155b2dccdf573e44
davidabrahams <da.j.abrahams@gmail.com>
2022-12-27 00:03:25 -0700

    Check in compiled code (#59)
    * commit compiled code
    
    * also assert compiled code is correct
    
    * use fullLinkJS instead
    
    * test, this should fail
    
    * Revert "test, this should fail"
    
    This reverts commit 96e8a31e2e930fd3088f4c33b662bbba0c734998.
    
    * idk
    
    * the mappings always change
    
    Co-authored-by: David Abrahams <davidabrahams@stripe.com>

--- scripts/scala.sh
+++ scripts/scala.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-prev=$(tar -cf - src | md5sum)
+prev=$(tar -cf - src target/scala-3.2.1/blockjumper-opt/main.js | md5sum)
 echo "Previous hash: $prev"
 check_equal_hashes () {
-  curr=$(tar -cf - src | md5sum)
+  curr=$(tar -cf - src target/scala-3.2.1/blockjumper-opt/main.js | md5sum)
   echo "New hash: $curr"
   if [ "$curr" != "$prev" ]; then
     echo "Source code changed. Run bash ./scripts/scala.sh"
@@ -13,7 +13,7 @@ check_equal_hashes () {
   fi
 }
 echo "Compiling Scala, fixing intentation..."
-sbt -Dscala.rewrite=indent "compile; test:compile; scalafmtAll"
+sbt -Dscala.rewrite=indent "fullLinkJS; test:compile; scalafmtAll"
 echo "Compiling Scala, fixing syntax..."
-sbt -Dscala.rewrite=new-syntax "compile; test:compile; scalafmtAll"
+sbt -Dscala.rewrite=new-syntax "fullLinkJS; test:compile; scalafmtAll"
 check_equal_hashes
